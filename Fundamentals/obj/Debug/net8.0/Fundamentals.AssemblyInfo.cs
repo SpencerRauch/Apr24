d59ec3efe254baa01a51e4189adc42a0e16d0efc
Spencer Rauch <rauchs@gmail.com>
2024-04-09 08:48:41 -0700

    gitignore

--- Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
+++ Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba05ccceb5ebe90733746b1d49f0dcb276a0d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0be591da25f23a4acc35288f0f96befa097a31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
